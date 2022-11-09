c5209fb6adc6cd7142a326997a34e4fa5e3c4066
dsdhumale <d.sdhumale@gmail.com>
2022-11-09 19:03:07 +0530

    Added : Given sad message constructor should return sad

--- main/java/com/Bridgelabz/MoodAnalyser.java
+++ main/java/com/Bridgelabz/MoodAnalyser.java
@@ -1,13 +1,31 @@
 package com.Bridgelabz;
 
 public class MoodAnalyser {
-    String mood;
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser Problem");
-    }
-    public String analysisMood(String message) {
-        if (message.equalsIgnoreCase("I am in any Mood"))
-            mood =  "HAPPY";
-        return mood;
-    }
+        //Declare variable
+        String mood;
+        String message;
+
+        //Default constructor
+    public MoodAnalyser(){
+
+        }
+        //Parameterised constructor
+    public MoodAnalyser(String message){
+            this.message = message;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Mood Analyser Problem");
+        }
+
+        //Method that return mood
+        public String analysisMood(String message) {
+            if (message.equalsIgnoreCase("I am in sad Mood")) {
+                mood = "SAD";
+            }
+            else {
+                mood = "HAPPY";
+            }
+            return mood;
+        }
 }
