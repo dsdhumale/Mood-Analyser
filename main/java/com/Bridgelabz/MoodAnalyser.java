495a6d7461d80cf2d860e0b63648e4bf9eb4e06e
dsdhumale <d.sdhumale@gmail.com>
2022-11-09 18:34:58 +0530

    Added : Ability to analyse and respond happy or sad mood

--- main/java/com/Bridgelabz/MoodAnalyser.java
+++ main/java/com/Bridgelabz/MoodAnalyser.java
@@ -1,8 +1,16 @@
 package com.Bridgelabz;
 
 public class MoodAnalyser {
-
+    String mood;
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyser Problem");
+    }
+    public String analysisMood(String message) {
+        if (message.contains("sad"))
+        { mood =  "SAD";
+        }
+        else{mood = "Happy";
+        }
+        return mood;
     }
 }
--- test/java/com/Bridgelabz/MoodAnalyserTest.java
+++ test/java/com/Bridgelabz/MoodAnalyserTest.java
@@ -2,5 +2,11 @@ package com.Bridgelabz;
 import org.junit.Assert;
 import org.junit.Test;
 public class MoodAnalyserTest {
-
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+    @Test
+    public void testMoodAnalysis(){
+        String message = "I am in sad Mood";
+        String mood = moodAnalyser.analysisMood(message);
+        Assert.assertEquals("SAD",mood);
+    }
 }
